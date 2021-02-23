836eb7b5d9d63ec37798809c62b84f62065789bd
CamiloSinningUN <61607058+CamiloSinningUN@users.noreply.github.com>
2021-02-23 11:20:40 -0500

    backup

---
+++ src/sinninglopezcamilojose/Viewer.java
@@ -0,0 +1,17 @@
+package sinninglopezcamilojose;
+
+import java.math.BigDecimal;
+
+public class Viewer {
+    public BigDecimal idViewer;
+    public String nickName;
+    public float Hours;
+
+    public Viewer(BigDecimal idViewer, String nickName, float Hours) {
+        this.idViewer = idViewer;
+        this.nickName = nickName;
+        this.Hours = Hours;
+    }
+    
+    
+}
